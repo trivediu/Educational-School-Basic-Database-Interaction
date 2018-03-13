657db095d9504bb2a47024f6e0b063e6dad50362
trivediu <31199718+trivediu@users.noreply.github.com>
2018-03-13 14:14:59 -0700

    Basic Working Version
    Reads data and inputs data into table

--- dbcon.js
+++ dbcon.js
@@ -1,13 +1,9 @@
-//Jay Swaminarayan - Swami Shreeji
-// Controls if quick suggestions should show up or not while typing
-
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs290_trivediu',
-    password        : '2126',
-    database        : 'cs290_trivediu'
+  connectionLimit : 10,
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs290_trivediu',
+  password        : '2126',
+  database        : 'cs290_trivediu'
 });
-
 module.exports.pool = pool;
